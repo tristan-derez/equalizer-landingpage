bc1098066773e7faa8fe6ecf2c862e9371970f0b
dreyzu <tristan.derez@gmail.com>
2022-03-02 12:47:53 +0100

    remove height 100%

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,7 +2,7 @@ import { Center, Text } from "@chakra-ui/react";
 
 export const NavBar = () => {
     return (
-        <Center h="100%">
+        <Center>
             <Text>Hello!</Text>
         </Center>
     );
