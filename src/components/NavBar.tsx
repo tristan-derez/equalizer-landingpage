a148e7bc39696bf869e1e68723345082cb9a7be5
dreyzu <tristan.derez@gmail.com>
2022-03-02 12:38:37 +0100

    create navbar component

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,9 @@
+import { Center, Text } from "@chakra-ui/react";
+
+export const NavBar = () => {
+    return (
+        <Center h="100%">
+            <Text>Hello!</Text>
+        </Center>
+    );
+};
