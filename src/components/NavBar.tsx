3a834d799163e16437e3057825a609bbd9531a15
dreyzu <tristan.derez@gmail.com>
2022-03-08 15:32:56 +0100

    remove navbar component

--- src/components/NavBar.tsx
+++
@@ -1,9 +0,0 @@
-import { Flex, Image } from "@chakra-ui/react";
-
-export const NavBar = () => {
-    return (
-        <Flex>
-            <Image src="/images/logo.svg" alt="logo" pl="24px" pt="39px" />
-        </Flex>
-    );
-};
