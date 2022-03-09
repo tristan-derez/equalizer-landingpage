a9fd0aa22b3ae4b5d8095d42767fd5d6b9809bc5
dreyzu <tristan.derez@gmail.com>
2022-03-09 10:58:40 +0100

    add responsive values to fontSize and lineHeight

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -20,10 +20,22 @@ export const Header = () => {
                 color="marine-blue"
                 w={["100%", "100%", "520px", "1015px"]}
             >
-                <Heading as="h1" fontWeight="700" fontSize="40px" lineHeight="48px" letterSpacing="-0.45px">
+                <Heading
+                    as="h1"
+                    fontWeight="700"
+                    fontSize={["40px", "40px", "64px", "88px"]}
+                    lineHeight={["48px", "44px", "64px", "88px"]}
+                    letterSpacing={["-0.45px", "-0.45px", "-0.73px", "-1px"]}
+                >
                     We make your music sound extraordinary.
                 </Heading>
-                <Text pt="20px" fontWeight="400" fontSize="16px" lineHeight="26px">
+                <Text
+                    w={["auto", "auto", "auto", "825px"]}
+                    pt="20px"
+                    fontWeight="400"
+                    fontSize={["16px", "16px", "18px", "20px"]}
+                    lineHeight={["26px", "26px", "28px", "34px"]}
+                >
                     A system audio equalizer specifically designed for Android and iOS. Freely tune the way your music
                     sounds with a professional grade parametric EQ & volume mixer. Control bass, mids, treble, gain
                     control, reverb, and more!
