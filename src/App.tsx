85dfb7f1e604ef05e50b991d086f3fe8c38bff1d
dreyzu <tristan.derez@gmail.com>
2022-03-09 12:50:43 +0100

    add Image and a br tag to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 
-import { ChakraProvider, Flex, extendTheme } from "@chakra-ui/react";
+import { ChakraProvider, Flex, Image } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "react-query";
 
 import { Footer } from "./components/Footer";
@@ -15,17 +15,36 @@ function App() {
     return (
         <QueryClientProvider client={queryClient}>
             <ChakraProvider theme={theme}>
-                <Flex
-                    direction="column"
-                    boxSize="100%"
-                    bgImage={[
-                        "url('./images/bg-main-mobile.png')",
-                        "url('./images/bg-main-tablet.png')",
-                        "url('./images/bg-main-desktop.png')",
-                    ]}
-                    bgPosition="top"
-                    bgRepeat="no-repeat"
-                >
+                <Flex direction="column" boxSize="100%">
+                    <Image
+                        src="./images/bg-main-mobile.png"
+                        alt="background"
+                        position="absolute"
+                        zIndex="-1"
+                        display={["block", "block", "none", "none"]}
+                        top="-90px"
+                        right="0px"
+                    />
+                    <Image
+                        src="./images/bg-main-tablet.png"
+                        alt="background"
+                        position="absolute"
+                        zIndex="-1"
+                        display={["none", "none", "block", "none"]}
+                        top="-50px"
+                        left="0"
+                        w="100%"
+                    />
+                    <Image
+                        src="./images/bg-main-desktop.png"
+                        alt="background"
+                        position="absolute"
+                        zIndex="-1"
+                        display={["none", "none", "none", "block"]}
+                        top="0px"
+                        left="-200px"
+                        w="100%"
+                    />
                     <Header />
                     <Main />
                     <Footer />
